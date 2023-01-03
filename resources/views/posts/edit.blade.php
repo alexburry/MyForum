9a3e8b3b0f4b0c9274ab1c3f976433c6fec2e8a1
AlexBurry <alexjburry@gmail.com>
2023-01-03 17:25:35 +0000

    fixed posts editing layout

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,27 +1,20 @@
 @extends('layouts.app')
-    @section('title', 'edit')
+    @section('title', 'Edit Post')
     @section('content')
     <div>
         <form method="POST" action="{{ route('posts.update', ['post'=>$post]) }}" class="mt-6 space-y-6">
             @csrf
             <div>
-                <x-input-label for="title" :value="__('Post Title')" />
-                <x-text-input id="title" class="block mt-1 w-full" type="text" name="title" :value="old('title')"/>
-                <x-input-error :messages="$errors->get('title')" class="mt-2" />
-            </div>
-            <div>
-                <x-input-label for="content" :value="__('Post Body')" />
-                <x-text-input id="content" class="block mt-1 w-full" type="text" name="content" :value="old('content')"/>
-                <x-input-error :messages="$errors->get('content')" class="mt-2" />
-            </div>
-            <div>
-                {{-- <p>User: <input type="text" name="user_id"
-                    value="{{ old('user_id') }}"></p> --}}
-                {{-- <input type="hidden" name="user_id"
-                    value="{{ Auth::id() }}"">
-                <input type="hidden" name="subforum_id"
-                    value="{{$subforum->id}}""> --}}
-                <input type="submit" value="Submit">
+                <form method="POST" action="{{ route('posts.store') }}" class="mt-6 space-y-6">
+                    @csrf
+                    <div><p>Title: <input type="text" name="title" class="input-text-box-title" value="{{ $post->title }}"> </p></div>
+                    <div><p>Content: <textarea type="text" name="content" class="input-text-area-content">{{ $post->content }} </textarea></p></div>
+                    
+                    <input type="hidden" name="user_id" value="{{ Auth::id() }}"">
+                    <input type="hidden" name="subforum_id" value="{{$subforum->id}}"">
+                    <input type="submit" value="Submit">
+                    </div>
+                </form>
             </div>
         </form>
     </div>
